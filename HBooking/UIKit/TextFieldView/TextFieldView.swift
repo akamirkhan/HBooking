4ffd3367835b968608df0786535fe6aa674f6055
Akaev Amirkhan <aak82183@gmail.com>
2023-12-24 00:14:39 +0300

    Update TextFieldView

--- HBooking/UIKit/TextFieldView/TextFieldView.swift
+++ HBooking/UIKit/TextFieldView/TextFieldView.swift
@@ -13,7 +13,7 @@ struct TextFieldView: View {
             isDataValid: $viewModel.isDataValid,
             title: viewModel.title
         ) { _ in
-            viewModel.isDataValid = false
+            viewModel.isDataValid = true
         }
     }
     
@@ -21,35 +21,7 @@ struct TextFieldView: View {
 
 #Preview {
     StatefulPreviewWrapper("") { text in
-        TextFieldView(viewModel: .init(title: "Имя", text: "", fieldsValidator: .init()))
+        TextFieldView(viewModel: .init(title: "Имя", text: "", fieldsValidator: .init()) { _ in })
             .padding(.horizontal, 16)
-    }
-}
-
-struct TextFieldFormat {
-    
-    static func format(_ mask: TextFieldMask, text: String) -> String {
-        let numbers = text.replacingOccurrences(of: "[^0-9]", with: "", options: .regularExpression)
-        var result = text.count == 1 ? "+7" : ""
-        var index = numbers.startIndex
-
-        for character in mask.rawValue where index < numbers.endIndex {
-            if character == "X" {
-                result.append(numbers[index])
-                index = numbers.index(after: index)
-            } else {
-                result.append(character)
-            }
-        }
-        
-        return result
-    }
-}
-
-enum TextFieldMask: String {
-    case phone = "+X (XXX) XXX-XX-XX"
-    
-    var symbols: Int {
-        self.rawValue.count
     }
 }
--- HBooking/UIKit/TextFieldView/TextFieldViewModel.swift
+++ HBooking/UIKit/TextFieldView/TextFieldViewModel.swift
@@ -5,17 +5,32 @@ final class TextFieldViewModel: ObservableObject {
     @Published var isDataValid = true
     @Published var text: String
     
-    private var cancellable: AnyCancellable?
+    var cancellables: Set<AnyCancellable> = []
     let title: String
     let fieldsValidator: PassthroughSubject<Void, Never>
     
-    init(title: String, text: String, fieldsValidator: PassthroughSubject<Void, Never>) {
+    init(
+        title: String,
+        text: String,
+        fieldsValidator: PassthroughSubject<Void, Never>,
+        updateText: @escaping (String) -> Void
+    ) {
         self.text = text
         self.title = title
         self.fieldsValidator = fieldsValidator
         
-        cancellable = fieldsValidator.sink { [unowned self] _ in
-            isDataValid = text.isNotEmpty
+        self.fieldsValidator.sink { [unowned self] _ in
+            isDataValid = self.text.isNotEmpty
         }
+        .store(in: &cancellables)
+        
+        $text
+            .dropFirst()
+            .receive(on: DispatchQueue.main)
+            .sink { updatedText in
+                updateText(updatedText)
+            }
+            .store(in: &cancellables)
     }
+
 }
